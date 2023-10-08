56937d9bf3c28e11e62bad10417b6407b764a525
petar-omni <117348982+petar-omni@users.noreply.github.com>
2023-10-08 20:00:01 +0200

    fix(): import val (#40)

--- src/pages/details/positions-page/positions.page.tsx
+++ src/pages/details/positions-page/positions.page.tsx
@@ -19,32 +19,111 @@ export const PositionsPage = () => {
 
   const getContent = () => {
     if (positionsData.isLoading && positionsData.isFetching && isConnected) {
+      return <FallbackContent type="spinner" />;
+    } else if (!isConnected) {
+      return <FallbackContent type="not_connected" />;
+    } else if (positionsData.isError) {
+      return <FallbackContent type="something_wrong" />;
+    } else if (isConnected && !positionsData.data?.length) {
+      return <FallbackContent type="no_current_positions" />;
+    }
+
+    return null;
+  };
+
+  const showPositions =
+    isConnected &&
+    (positionsData.data.length ||
+      (!positionsData.isLoading && !positionsData.isError));
+
+  return (
+    <PageContainer>
+      <Box display="flex" flex={1} flexDirection="column">
+        {getContent()}
+
+        {showPositions && (
+          <Box flex={1} display="flex" width="full">
+            <VirtualList
+              className={virtuosoContainer}
+              data={["header" as const, ...positionsData.data]}
+              itemContent={(_index, item) =>
+                item === "header" ? (
+                  <ListItem variant={{ hover: "disabled" }}>
+                    <Box
+                      display="flex"
+                      justifyContent="space-between"
+                      alignItems="center"
+                      gap="2"
+                      marginBottom="2"
+                    >
+                      <Box
+                        display="flex"
+                        flexDirection="column"
+                        gap="2"
+                        flex={2}
+                      >
+                        <Text variant={{ weight: "bold" }}>
+                          {t("positions.dont_see_position")}
+                        </Text>
+
+                        <Text variant={{ weight: "normal", type: "muted" }}>
+                          {t("positions.import_validator")}
+                        </Text>
+                      </Box>
+
+                      <Box
+                        flex={1}
+                        display="flex"
+                        justifyContent="flex-end"
+                        alignItems="center"
+                      >
+                        <ImportValidator {...importValidators} />
+                      </Box>
+                    </Box>
+                  </ListItem>
+                ) : (
+                  <PositionsListItem item={item} />
+                )
+              }
+            />
+          </Box>
+        )}
+      </Box>
+    </PageContainer>
+  );
+};
+
+const FallbackContent = ({
+  type,
+}: {
+  type:
+    | "not_connected"
+    | "no_current_positions"
+    | "spinner"
+    | "something_wrong";
+}) => {
+  const { t } = useTranslation();
+
+  const getContent = () => {
+    if (type === "spinner") {
       return (
-        <Box my="2" display="flex" flex={1} justifyContent="center">
+        <Box display="flex" justifyContent="center">
           <Spinner />
         </Box>
       );
-    } else if (!isConnected) {
+    } else if (type === "not_connected") {
+      return <ConnectButton />;
+    } else if (type === "something_wrong") {
       return (
-        <Box my="2" flex={1}>
-          <ConnectButton />
-        </Box>
+        <Text variant={{ type: "danger" }} textAlign="center">
+          {t("shared.something_went_wrong")}
+        </Text>
       );
-    } else if (positionsData.isError) {
+    } else if (type === "no_current_positions") {
       return (
-        <Box display="flex" justifyContent="center" my="2">
-          <Text variant={{ type: "danger" }}>
-            {t("shared.something_went_wrong")}
-          </Text>
-        </Box>
-      );
-    } else if (isConnected && !positionsData.data?.length) {
-      return (
-        <Box my="2">
-          <Text variant={{ weight: "medium" }}>
-            {t("positions.no_current_positions")}
-          </Text>
-        </Box>
+        <Text variant={{ weight: "medium" }} textAlign="center">
+          {t("positions.no_current_positions")}
+        </Text>
       );
     }
 
@@ -52,53 +131,9 @@ export const PositionsPage = () => {
   };
 
   return (
-    <PageContainer>
-      <Box display="flex" justifyContent="center" flex={1}>
-        {getContent()}
-
-        {!!positionsData.data?.length && (
-          <VirtualList
-            className={virtuosoContainer}
-            style={{ height: "auto" }}
-            data={["header" as const, ...positionsData.data]}
-            itemContent={(_index, item) =>
-              item === "header" ? (
-                <ListItem variant={{ hover: "disabled" }}>
-                  <Box
-                    display="flex"
-                    justifyContent="space-between"
-                    alignItems="center"
-                    gap="2"
-                    marginBottom="2"
-                  >
-                    <Box display="flex" flexDirection="column" gap="2" flex={2}>
-                      <Text variant={{ weight: "bold" }}>
-                        {t("positions.dont_see_position")}
-                      </Text>
-
-                      <Text variant={{ weight: "normal", type: "muted" }}>
-                        {t("positions.import_validator")}
-                      </Text>
-                    </Box>
-
-                    <Box
-                      flex={1}
-                      display="flex"
-                      justifyContent="flex-end"
-                      alignItems="center"
-                    >
-                      <ImportValidator {...importValidators} />
-                    </Box>
-                  </Box>
-                </ListItem>
-              ) : (
-                <PositionsListItem item={item} />
-              )
-            }
-          />
-        )}
-      </Box>
-    </PageContainer>
+    <Box marginTop="2" marginBottom="4">
+      {getContent()}
+    </Box>
   );
 };
 
