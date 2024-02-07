b05d3a1123c470d78d8f2c9845fca560532779ab
petar-omni <117348982+petar-omni@users.noreply.github.com>
2024-02-07 12:30:20 +0100

    feat(): delay api requests (#97)
    * fix(): margin
    
    * feat(): delay api requests

--- src/components/atoms/select-modal/styles.css.ts
+++ src/components/atoms/select-modal/styles.css.ts
@@ -13,7 +13,6 @@ const fadeIn = keyframes({
 });
 
 export const container = style({
-  marginRight: "var(--removed-body-scroll-bar-size)",
   zIndex: 2,
   display: "flex",
   justifyContent: "center",
