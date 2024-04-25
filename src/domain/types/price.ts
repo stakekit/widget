2107fdf7b6a375a7e0b64c88caa8b56f10878d7c
petar-omni <117348982+petar-omni@users.noreply.github.com>
2024-04-25 16:58:36 +0200

    ENG-2371, ENG-2370: Description + position price (#171)
    * feat(): update resting description
    
    * fix(): prioritise pricePerShare

--- src/domain/types/price.ts
+++ src/domain/types/price.ts
@@ -1,8 +1,17 @@
+import type { TokenDto } from "@stakekit/api-hooks";
 import type { TokenString } from "./tokens";
+import { tokenString } from "..";
+import { Maybe } from "purify-ts";
 
 export type Price = {
   price: number | undefined;
   price24H: number | undefined;
 };
 
-export type Prices = Map<TokenString, Price>;
+export class Prices {
+  constructor(public value: Map<TokenString, Price>) {}
+
+  getByToken(token: TokenDto) {
+    return Maybe.fromNullable(this.value.get(tokenString(token)));
+  }
+}
--- src/utils/mappers.ts
+++ src/utils/mappers.ts
@@ -1,10 +1,6 @@
-import type { Networks, Token } from "@stakekit/common";
-import type { Price, Prices, TokenString } from "../domain/types";
-import type {
-  PriceResponseDto,
-  TokenDto,
-  Networks as NetworksDto,
-} from "@stakekit/api-hooks";
+import { Prices } from "../domain/types";
+import type { Price, TokenString } from "../domain/types";
+import type { PriceResponseDto } from "@stakekit/api-hooks";
 
 const priceDtoToPrice = (priceDto: PriceResponseDto[string]): Price => ({
   price: priceDto.price,
@@ -13,19 +9,11 @@ const priceDtoToPrice = (priceDto: PriceResponseDto[string]): Price => ({
 
 export const priceResponseDtoToPrices = (
   priceResponseDto: PriceResponseDto
-): Prices => {
-  return Object.keys(priceResponseDto).reduce<Prices>((acc, key) => {
-    acc.set(key as TokenString, priceDtoToPrice(priceResponseDto[key]));
+): Prices =>
+  new Prices(
+    Object.keys(priceResponseDto).reduce<Prices["value"]>((acc, key) => {
+      acc.set(key as TokenString, priceDtoToPrice(priceResponseDto[key]));
 
-    return acc;
-  }, new Map());
-};
-
-const networksToNetworksDto = (networks: Networks): NetworksDto => {
-  return networks as NetworksDto;
-};
-
-export const tokenToTokenDto = (token: Token): TokenDto => ({
-  ...token,
-  network: networksToNetworksDto(token.network),
-});
+      return acc;
+    }, new Map())
+  );
