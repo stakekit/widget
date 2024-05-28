a365cc0188495d1149bc80c7d6d684aa4f4f357d
Stefan Parezanovic <33752425+Stefanats@users.noreply.github.com>
2024-05-28 11:39:55 +0200

    test(): mocking initial load

--- tests/use-cases/renders-initial-page.test.tsx
+++ tests/use-cases/renders-initial-page.test.tsx
@@ -1,8 +1,123 @@
 import { describe, expect, it } from "vitest";
 import { renderApp, waitFor } from "../utils/test-utils";
+import type { TokenDto, YieldDto } from "@stakekit/api-hooks";
+import { getYieldControllerYieldOpportunityResponseMock } from "@stakekit/api-hooks/msw";
+import { http, HttpResponse, delay } from "msw";
+import { Just } from "purify-ts";
+import { server } from "../mocks/server";
 
 describe("Renders initial page", () => {
   it("Works as expected", async () => {
+
+    const avalancheCToken: TokenDto = {
+      name: "Avalanche C Chain",
+      symbol: "AVAX",
+      decimals: 18,
+      network: "avalanche-c",
+      coinGeckoId: "avalanche-2",
+      logoURI: "https://assets.stakek.it/tokens/avax.svg",
+    };
+
+    const ether: TokenDto = {
+      network: "ethereum",
+      name: "Ethereum",
+      symbol: "ETH",
+      decimals: 18,
+      coinGeckoId: "ethereum",
+      logoURI: "https://assets.stakek.it/tokens/eth.svg",
+    };
+
+    const avalancheAvaxNativeStaking = Just(
+      getYieldControllerYieldOpportunityResponseMock()
+    )
+      .map(
+        (val) =>
+          ({
+            ...val,
+            id: "avalanche-avax-native-staking",
+            token: avalancheCToken,
+            tokens: [avalancheCToken],
+            status: { enter: true, exit: true },
+            args: { enter: { args: { nfts: undefined } } },
+            metadata: {
+              ...val.metadata,
+              type: "staking",
+              gasFeeToken: avalancheCToken,
+            },
+          }) satisfies YieldDto
+      )
+      .unsafeCoerce();
+
+    const etherNativeStaking = Just(
+      getYieldControllerYieldOpportunityResponseMock()
+    )
+      .map(
+        (val) =>
+          ({
+            ...val,
+            id: "ethereum-eth-etherfi-staking",
+            token: ether,
+            tokens: [ether],
+            status: { enter: true, exit: true },
+            args: { enter: { args: { nfts: undefined } } },
+            metadata: {
+              ...val.metadata,
+              type: "staking",
+              gasFeeToken: ether,
+            },
+          }) satisfies YieldDto
+      )
+      .unsafeCoerce();
+
+    server.use(
+      http.get("*/v1/yields/enabled/networks", async () => {
+        await delay();
+        return HttpResponse.json([
+          etherNativeStaking.token.network,
+          avalancheAvaxNativeStaking.token.network,
+        ]);
+      }),
+
+      http.get("*/v1/tokens", async () => {
+        await delay();
+
+        return HttpResponse.json([
+          { token: ether, availableYields: [etherNativeStaking.id] },
+          {
+            token: avalancheCToken,
+            availableYields: [avalancheAvaxNativeStaking.id],
+          },
+        ]);
+      }),
+
+      http.post("*/v1/tokens/balances/scan", async () => {
+        await delay();
+        return HttpResponse.json([
+          {
+            token: ether,
+            amount: "3",
+            availableYields: [etherNativeStaking.id],
+          },
+          {
+            token: avalancheCToken,
+            amount: "3",
+            availableYields: [avalancheAvaxNativeStaking.id],
+          },
+        ]);
+      }),
+
+      http.get(`*/v1/yields/${etherNativeStaking.id}`, async () => {
+        await delay();
+
+        return HttpResponse.json(etherNativeStaking);
+      }),
+      http.get(`*/v1/yields/${avalancheAvaxNativeStaking.id}`, async () => {
+        await delay();
+
+        return HttpResponse.json(avalancheAvaxNativeStaking);
+      })
+    );
+
     const { queryByText, queryByTestId } = renderApp();
 
     await waitFor(() =>
