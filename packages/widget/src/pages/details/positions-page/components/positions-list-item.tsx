83798a1a9eb74de663592c6f2f6d4911f2f9ec89
Stefan Parezanovic <33752425+Stefanats@users.noreply.github.com>
2024-06-24 17:20:36 +0200

    fix(): display proper name on position (#218)
    * fix(): display proper name on position
    
    * fix(): priority order enum
    
    * fix(): position item points without amount
    
    ---------
    
    Co-authored-by: Petar Todorovic <petar@omni.app>

--- packages/widget/src/pages/details/positions-page/components/positions-list-item.tsx
+++ packages/widget/src/pages/details/positions-page/components/positions-list-item.tsx
@@ -1,5 +1,6 @@
+import type { YieldBalanceDto } from "@stakekit/api-hooks";
 import BigNumber from "bignumber.js";
-import { List, Maybe } from "purify-ts";
+import { List, Maybe, compare } from "purify-ts";
 import { memo, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { Box, Spinner, Text } from "../../../../components";
@@ -21,6 +22,17 @@ import {
 import { ImportValidator } from "./import-validator";
 import { listItem, noWrap } from "./styles.css";
 
+const priorityOrder: { [key in YieldBalanceDto["type"]]: number } = {
+  available: 1,
+  staked: 2,
+  unstaking: 3,
+  unstaked: 4,
+  preparing: 5,
+  locked: 6,
+  unlocking: 7,
+  rewards: 8,
+};
+
 export const PositionsListItem = memo(
   ({
     item,
@@ -29,12 +41,14 @@ export const PositionsListItem = memo(
   }) => {
     const { t } = useTranslation();
 
-    const actionRequired = useMemo(() => {
-      return (
+    const actionRequired = useMemo(
+      () =>
         item.type === "default" &&
-        item.balances.some((b) => b.type === "locked" || b.type === "unstaked")
-      );
-    }, [item.balances, item.type]);
+        item.balancesWithAmount.some(
+          (b) => b.type === "locked" || b.type === "unstaked"
+        ),
+      [item.balancesWithAmount, item.type]
+    );
 
     const yieldOpportunity = useYieldOpportunity(item.integrationId);
 
@@ -45,24 +59,33 @@ export const PositionsListItem = memo(
 
     const amount = useMemo(
       () =>
-        item.balances.reduce((acc, b) => {
+        item.balancesWithAmount.reduce((acc, b) => {
           if (b.token.isPoints) return acc;
 
           return new BigNumber(b.amount).plus(acc);
         }, new BigNumber(0)),
-      [item.balances]
+      [item.balancesWithAmount]
     );
 
     const pointsRewardTokenBalance = useMemo(
-      () => List.find((v) => !!v.token.isPoints, item.balances),
-      [item.balances]
+      () => List.find((v) => !!v.token.isPoints, item.balancesWithAmount),
+      [item.balancesWithAmount]
     );
 
-    const token = List.head(item.balances).map((v) => v.token);
+    const token = useMemo(
+      () =>
+        List.head(
+          List.sort(
+            (a, b) => compare(priorityOrder[a.type], priorityOrder[b.type]),
+            item.allBalances
+          )
+        ).map((v) => v.token),
+      [item.allBalances]
+    );
 
     const hasPendingClaimRewards = useMemo(
-      () => checkHasPendingClaimRewards(item.balances),
-      [item.balances]
+      () => checkHasPendingClaimRewards(item.balancesWithAmount),
+      [item.balancesWithAmount]
     );
 
     const providersDetails = useProvidersDetails({
@@ -98,7 +121,7 @@ export const PositionsListItem = memo(
       () =>
         providersDetails
           .chain((val) => List.find((v) => v.status !== "active", val))
-          .chain((val) => Maybe.fromNullable(val.status))
+          .chainNullable((val) => val.status)
           .map((v) => v as Exclude<typeof v, "active">)
           .extractNullable(),
       [providersDetails]
