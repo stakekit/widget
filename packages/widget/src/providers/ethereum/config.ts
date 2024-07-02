7692fd467ab655200ac9b0f74120f663119435ba
Philip Zudemberg <philip.zdb@gmail.com>
2024-07-02 14:01:03 +0200

    feat: add base support to widget

--- packages/widget/src/providers/ethereum/config.ts
+++ packages/widget/src/providers/ethereum/config.ts
@@ -14,6 +14,7 @@ import { EitherAsync, Maybe } from "purify-ts";
 import {
   arbitrum,
   avalanche,
+  base,
   bsc,
   celo,
   goerli,
@@ -96,6 +97,11 @@ const queryFn = async ({
             type: "evm",
             skChainName: EvmNetworks.Binance,
             wagmiChain: bsc,
+          },
+          [EvmNetworks.Base]: {
+            type: "evm",
+            skChainName: EvmNetworks.Base,
+            wagmiChain: base,
           },
           [EvmNetworks.EthereumHolesky]: {
             type: "evm",
