e97e72e6d695feec5e7bc0ecf2f7782180295722
Petar Todorovic <petar@omni.app>
2024-07-25 11:44:14 +0200

    refactor(): param-validation test

--- packages/widget/tests/use-cases/deep-links-flow/param-validation.test.ts
+++ packages/widget/tests/use-cases/deep-links-flow/param-validation.test.ts
@@ -5,76 +5,48 @@ import { setUrl as _setUrl } from "./utils";
 
 describe("Deep link param validation", () => {
   it("Should validate yieldId param", async () => {
-    let yieldId = "ethereum-eth-native-staking";
-    const assertIsValidPendingAction = (hasRes: boolean) =>
+    const setAndAssertIsValidYieldIdParam = (
+      yieldId: string,
+      valid: boolean
+    ) => {
+      _setUrl({ yieldId });
+
       expect(
         getAndValidateInitParams({ externalProviderInitToken: undefined })
           .map((v) => v.yieldId)
           .extract()
-      ).toEqual(hasRes ? yieldId : null);
-
-    const setYieldIdParam = (newYieldId: string) => {
-      yieldId = newYieldId;
-      _setUrl({ yieldId });
+      ).toEqual(valid ? yieldId : null);
     };
 
-    setYieldIdParam(yieldId);
-    assertIsValidPendingAction(true);
-
-    setYieldIdParam("../ethereum-eth-native-staking");
-    assertIsValidPendingAction(false);
-
-    setYieldIdParam("./ethereum-eth-native-staking");
-    assertIsValidPendingAction(false);
-
-    setYieldIdParam("ethereum-../eth-native-staking");
-    assertIsValidPendingAction(false);
-
-    setYieldIdParam("ethereum-eth-native-staking../");
-    assertIsValidPendingAction(false);
-
-    setYieldIdParam("ethereum-eth-native-staking/../");
-    assertIsValidPendingAction(false);
+    setAndAssertIsValidYieldIdParam("ethereum-eth-native-staking", true);
+    setAndAssertIsValidYieldIdParam("../ethereum-eth-native-staking", false);
+    setAndAssertIsValidYieldIdParam("./ethereum-eth-native-staking", false);
+    setAndAssertIsValidYieldIdParam("ethereum-../eth-native-staking", false);
+    setAndAssertIsValidYieldIdParam("ethereum-eth-native-staking../", false);
+    setAndAssertIsValidYieldIdParam("ethereum-eth-native-staking/../", false);
   });
 
   it("Should validate pendingAction param", async () => {
-    let pendingaction: ActionTypes | (string & {}) = "";
-    const assertIsValidPendingAction = (hasRes: boolean) =>
+    const setAndAssertIsValidPendingActionParam = (
+      pendingaction: ActionTypes | (string & {}),
+      valid: boolean
+    ) => {
+      _setUrl({ pendingaction });
+
       expect(
         getAndValidateInitParams({ externalProviderInitToken: undefined })
           .map((v) => v.pendingaction)
           .extract()
-      ).toEqual(hasRes ? pendingaction : null);
-
-    const setPendingActionParam = (
-      newPendingActionParam: typeof pendingaction
-    ) => {
-      pendingaction = newPendingActionParam;
-      _setUrl({ pendingaction });
+      ).toEqual(valid ? pendingaction : null);
     };
 
-    setPendingActionParam("CLAIM_REWARDS");
-    assertIsValidPendingAction(true);
-
-    setPendingActionParam("STAKE");
-    assertIsValidPendingAction(true);
-
-    setPendingActionParam("RESTAKE_REWARDS");
-    assertIsValidPendingAction(true);
-
-    setPendingActionParam("../CLAIM_REWARDS");
-    assertIsValidPendingAction(false);
-
-    setPendingActionParam("./CLAIM_REWARDS");
-    assertIsValidPendingAction(false);
-
-    setPendingActionParam("ethereum-../STAKE");
-    assertIsValidPendingAction(false);
-
-    setPendingActionParam("STAKE../");
-    assertIsValidPendingAction(false);
-
-    setPendingActionParam("UNSTAKE/../");
-    assertIsValidPendingAction(false);
+    setAndAssertIsValidPendingActionParam("CLAIM_REWARDS", true);
+    setAndAssertIsValidPendingActionParam("STAKE", true);
+    setAndAssertIsValidPendingActionParam("RESTAKE_REWARDS", true);
+    setAndAssertIsValidPendingActionParam("../CLAIM_REWARDS", false);
+    setAndAssertIsValidPendingActionParam("./CLAIM_REWARDS", false);
+    setAndAssertIsValidPendingActionParam("ethereum-../STAKE", false);
+    setAndAssertIsValidPendingActionParam("STAKE../", false);
+    setAndAssertIsValidPendingActionParam("UNSTAKE/../", false);
   });
 });
