c5566e3b59b6ad3bfb4ac67f8d9b740bbf87338a
Stefan Parezanovic <33752425+Stefanats@users.noreply.github.com>
2024-07-25 15:45:56 +0200

    test(): geo block (#240)

---
+++ packages/widget/tests/use-cases/geo-block.test.tsx
@@ -0,0 +1,30 @@
+import { http, HttpResponse } from "msw";
+import { describe, expect, it } from "vitest";
+import { server } from "../mocks/server";
+import { renderApp, waitFor } from "../utils/test-utils";
+
+describe("Geo block", () => {
+  it("Show geo block popup", async () => {
+    server.use(
+      http.get("*/v1/yields/enabled/networks", async () => {
+        return HttpResponse.json(
+          {
+            code: 403,
+            countryCode: "AT",
+            message: "Access denied",
+            regionCode: "AT-9",
+            type: "GEO_LOCATION",
+          },
+          { status: 403 }
+        );
+      })
+    );
+
+    const { queryByText } = renderApp();
+
+    await waitFor(() => expect(queryByText("Geo Block")).toBeInTheDocument());
+    expect(
+      queryByText("This feature is not available in Austria.")
+    ).toBeInTheDocument();
+  });
+});
