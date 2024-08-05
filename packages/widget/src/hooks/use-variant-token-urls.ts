90f944b54b2f3ac3abea7fd7e7ed6fa1fc874eba
Petar Todorovic <petar@omni.app>
2024-08-05 23:22:28 +0200

    fix(): variant urls

--- packages/widget/src/hooks/use-variant-token-urls.ts
+++ packages/widget/src/hooks/use-variant-token-urls.ts
@@ -1,6 +1,7 @@
 import { config } from "@sk-widget/config";
 import { useSettings } from "@sk-widget/providers/settings";
 import type { TokenDto, YieldMetadataDto } from "@stakekit/api-hooks";
+import { Maybe } from "purify-ts";
 import { useMemo } from "react";
 
 export const useVariantTokenUrls = (
@@ -16,26 +17,36 @@ export const useVariantTokenUrls = (
 
   return useMemo(() => {
     if (metadata) {
+      const mainUrl = Maybe.fromFalsy(variant === "zerion")
+        .filter(() =>
+          skETHIconUrlsSuffix.some((v) => metadata.logoURI.endsWith(v))
+        )
+        .map(() => zerionETHIcon)
+        .orDefault(metadata.logoURI);
+
       return {
-        mainUrl: metadata.logoURI,
+        mainUrl,
         fallbackUrl: metadata.logoURI ?? token.logoURI,
         name: metadata.name,
         providerIcon: metadata.provider?.logoURI,
       };
     }
 
-    let mainUrl = token.logoURI;
+    const mainUrl = Maybe.fromFalsy(variant === "zerion")
+      .map(() => {
+        /**
+         * Use Zerion's token icons
+         */
+        if (token.address && token.symbol === "MATIC") {
+          return zerionMATICIcon(token.address);
+        }
 
-    if (variant === "zerion") {
-      /**
-       * Use Zerion's token icons
-       */
-      if (token.address && token.symbol === "MATIC") {
-        mainUrl = `${config.zerion.iconsByAddress}${token.address}.png`;
-      } else if (token.symbol === "ETH") {
-        mainUrl = `${config.zerion.iconsByAddress}eth.png`;
-      }
-    }
+        if (token.symbol === "ETH") {
+          return zerionETHIcon;
+        }
+        return token.logoURI;
+      })
+      .orDefault(token.logoURI);
 
     return {
       mainUrl,
@@ -45,3 +56,8 @@ export const useVariantTokenUrls = (
     };
   }, [token, metadata, variant]);
 };
+
+const skETHIconUrlsSuffix = ["/tokens/eth.svg", "/tokens/steth2.svg"];
+const zerionMATICIcon = (address: string) =>
+  `${config.zerion.iconsByAddress}${address}.png`;
+const zerionETHIcon = `${config.zerion.iconsByAddress}eth.png`;
